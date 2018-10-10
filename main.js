17cac1323c6eee657e270fcff933e55d969fee64
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-10-10 11:22:12 -0700

    Document ready function and moves variables

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(initializeApp);
+
+var topLeft = [-1,-1];
+var top = [-1,0];
+var topRight = [-1,1];
+var right = [0,1];
+var bottomRight = [1,1];
+var bottom = [1,0];
+var bottomLeft = [1,-1];
+var left = [0,-1]
+
+function initializeApp() {
+
+
+}
