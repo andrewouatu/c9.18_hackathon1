0797a82885131d3c4dade3a1eccfd643a477c273
julian so <julianso89@gmail.com>
2018-10-11 11:58:43 -0700

    changed HTML skeleton

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ $(document).ready(initializeApp);
 var moveDirectionsArray = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]];
 
 function initializeApp() {
-
     $('div').removeClass("black");
-    $('div').removeClass("white");
+    $('div').removeClass("white";
 }
-
 
 var gameBoard = [
                     ["0", "0", "0", "0","0", "0","0","0"],
@@ -20,6 +18,48 @@ var gameBoard = [
                     ["0", "0", "0", "0","0", "0","0","0"],
                     ["0", "0", "0", "0","0", "0","0","0"],
                 ];
+
+
+// function addClasstoChip(){
+//     $("#square" + row + "-" + column).addClass("black");
+//
+// }
+//
+// var currentLocation = [];
+//
+// function selectSquare(row, column) {
+//
+//     $("#square" + row + "-" + column).addClass("black");
+//     currentLocation.push(row);
+//     currentLocation.push(column);
+// }
+//
+// function addClass() {
+//     $("#chip").addClass("white");
+// }
+//
+// addClass();
+//
+// function addClick() {
+//     $('.square').click(chipClicked)
+// }
+//
+//
+// function chipClicked(){
+//     currentChip = event.currentTarget;
+//     $('.chip').addClass("black");
+// }
+// //
+//
+//
+// function showCard(card){
+//     $(card).addClass('hide');
+// }
+//
+// if(firstCardClicked === null){
+//     firstCardClicked = event.currentTarget;
+//     showCard(event.currentTarget);
+//
 
 
 // function addChip(row, col){
