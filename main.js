ed85a65a0836969be22cd625b620f5780670563a
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-10-11 12:26:55 -0700

    click-handlers and position data

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
 
 $(document).ready(initializeApp);
 
-var moveDirectionsArray = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]];
+
+var currentPlayerChipsPosition=[];
+var moveDirectionsArray = [
+    {row: -1, column: -1},
+    {row: -1, column: 0},
+    {row: -1, column: 1},
+    {row: 0, column: 1},
+    {row: 1, column: 1},
+    {row: 1, column: 0},
+    {row: 1, column: -1},
+    {row: 0, column: -1},
+]
+
+
+
 
 function initializeApp() {
+    $('.square').click(getClickedSquarePosition);
 
+    // var gameBoardArray = [
+    //     ["0", "0", "0", "0","0", "0","0","0"],
+    //     ["0", "0", "0", "0","0", "0","0","0"],
+    //     ["0", "0", "0", "0","0", "0","0","0"],
+    //     ["0", "0", "0", "1","2", "0","0","0"],
+    //     ["0", "0", "0", "2","1", "0","0","0"],
+    //     ["0", "0", "0", "0","0", "0","0","0"],
+    //     ["0", "0", "0", "0","0", "0","0","0"],
+    //     ["0", "0", "0", "0","0", "0","0","0"],
+    // ];
+    // findStartingChipsPositionForCurrentPlayer(gameBoardArray);
 
 }
 
+// var gameround = true
+// gameround = !gameround
 
+// function checkNextToCurrentPlayerColor(startPoint, userColor, opponentColor){
+//     var possibilities = [];
+//     while (var moveDirectionsArrayIndex=0; moveDirectionsArrayIndex<moveDirectionsArray.length; moveDirectionsArrayIndex++) {
+//
+//     while (gameBoardArray[spotCheckd])
+// }
 
-var gameBoard = [
+var gameBoardArray = [
                     ["0", "0", "0", "0","0", "0","0","0"],
                     ["0", "0", "0", "0","0", "0","0","0"],
                     ["0", "0", "0", "0","0", "0","0","0"],
@@ -22,3 +56,34 @@ var gameBoard = [
                 ];
 
 
+// function findStartingChipsPositionForCurrentPlayer (gameBoard){
+// debugger;
+//     for (var rowIndex = 0; rowIndex <8; rowIndex++){
+//         var currentChip = [];
+//         currentChip[0]=rowIndex;
+//         for (var columnIndex = 0; columnIndex<8; columnIndex++) {
+//             if(gameBoard[rowIndex][columnIndex]==='2'){
+//
+//
+//                 currentChip[1]=columnIndex;
+//                 currentPlayerChipsPosition.push(currentChip);
+//                 console.log(currentChip);
+//             }
+//
+//         }
+//     }
+//     return currentPlayerChipsPosition;
+// }
+// findStartingChipsPositionForCurrentPlayer(gameBoardArray);
+// console.log(currentPlayerChipsPosition);
+//
+//
+// }
+
+function getClickedSquarePosition(){
+    inputPositionArray=[];
+    inputPositionArray[0]=$(event.currentTarget).attr(data-row);
+    inputPositionArray[1]=$(event.currentTarget).attr(data-col);
+    console.log(inputPositionArray);
+
+}
